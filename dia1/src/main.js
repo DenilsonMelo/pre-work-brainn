e171dd3c4b54fe1267926c81aeca638db0485974
unknown <DenilsonMelo>
2021-08-17 14:14:06 -0300

    resolução exercicio 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,12 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+document.querySelector('[data-js="button"]').addEventListener('click', (event) => {
+  event.preventDefault()
+  if(document.querySelector('.app').style.display === 'none'){
+    document.querySelector('.app').style.display = 'block'
+  } else{
+    document.querySelector('.app').style.display = 'none'
+  }
+})
